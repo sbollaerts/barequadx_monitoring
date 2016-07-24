b57113c1fd8fd946417bca3dff233b45239b42d9
sbollaerts <sbollaerts@hotmail.com>
2016-07-24 11:36:16 +0200

    Removed "Calibration mode" (to focus on the bare functionalities of this project)

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ RX_SPEED = 115200
 DEBUG = True
 
 receiver = None
-range_min = [2048] * 4
-range_max = [0] * 4
-range_neutral = [1024] * 4
-range_value = [0] * 4
+input_min = [2048] * 4
+input_max = [0] * 4
+input_value = [0] * 4
+
+output_min = [2048] * 4
+output_max = [2048] * 4
+output_neutral = [0] * 4
 
 
 def compute_rate(range_min, range_max, value):
     if range_min == 0 | range_max == range_min:
         rc = 0
     else:
+        value = max(min(value, range_max), range_min)
         rc = ((value - range_min) / (range_max - range_min)) * 100
     return rc
 
@@ -32,15 +36,35 @@ receiver = Receiver(port=RX_PORT, speed=RX_SPEED, debug=False)
 try:
     while True:
         if receiver.read_serial():
+
             for i in range(0, 4):
                 if receiver.channels[i] > 0:
-                    range_value[i] = receiver.channels[i]
-                    range_min[i] = min(range_min[i], range_value[i])
-                    range_max[i] = max(range_max[i], range_value[i])
-                    range_neutral[i] = range_min[i] + (range_max[i] - range_min[i]) / 2
-                print("CH%.2d [%4d:%4d] (%4d) %4d -> %3d%%" % (
-                    i, range_min[i], range_max[i], range_neutral[i], range_value[i],
-                    compute_rate(range_min[i], range_max[i], range_value[i])), end=" || ")
+                    input_value[i] = receiver.channels[i]
+                    input_min[i] = min(input_min[i], input_value[i])
+                    input_max[i] = max(input_max[i], input_value[i])
+
+                    if output_neutral[i] == 0:
+                        output_neutral[i] = input_value[i]
+
+                    delta_low = output_neutral[i] - input_min[i]
+                    delta_high = input_max[i] - output_neutral[i]
+                    delta = min(delta_low, delta_high)
+
+                    output_min[i] = output_neutral[i] - delta
+                    output_max[i] = output_neutral[i] + delta
+
+                print("CH%d [%d<%4d<%d] %3d%% - (%d:%d) %3d%%" % (
+                    i,
+                    input_min[i], input_value[i], input_max[i],
+                    compute_rate(input_min[i], input_max[i], input_value[i]),
+                    output_min[i], output_max[i],
+                    compute_rate(output_min[i], output_max[i], input_value[i])), end=" || ")
+                # print("CH%d [%d<%d<%d] %3d%% - (%d<%d<%d) %3d%%" % (
+                #     i,
+                #     input_min[i], input_value[i], input_max[i],
+                #     compute_rate(input_min[i], input_max[i], input_value[i]),
+                #     output_min[i], output_neutral[i], output_max[i],
+                #     compute_rate(output_min[i], output_max[i], input_value[i])), end=" || ")
             print("")
 except KeyboardInterrupt:
     receiver = None
